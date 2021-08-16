48fe38333de5d09462f1c54de023df7018c99bf6
kunalgangaher17 <gangaherkunal@gmail.com>
2021-08-16 18:10:08 +0530

    test commit

---
+++ Test/src/main/java/com/vmware/test/ShapeConstants.java
@@ -0,0 +1,9 @@
+package com.vmware.test;
+
+public class ShapeConstants {
+
+	public static final String CIRCLE="circle";
+	public static final String RECTANGLE="rectangle";
+	public static final String SQUARE="square";
+
+}
